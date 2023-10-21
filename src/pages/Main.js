41944ab8a9f8359a7cda8c7a900971b0e8d12cd6
treva556 <marktreva92@gmail.com>
2023-10-22 00:03:21 +0300

    body and header

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,9 +3,9 @@ import React from "react";
 function Main() {
   return (
     <div className="grid grid-cols-2 gap-4">
-        <div className="box-border h-64 w-110 p-4 border-4">
+        <div className="box-border h-64 w-110 p-4 items-center">
 
-            <p>pic1</p>
+            <p className=" text-center">pic1</p>
 
         </div>
         <div>

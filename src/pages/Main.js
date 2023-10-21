07913b3e58335cce024c0161ba4b1d9a71fc3925
treva556 <marktreva92@gmail.com>
2023-10-21 23:24:28 +0300

    setup

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,8 +2,10 @@ import React from "react";
 
 function Main() {
   return (
-    <div className="App">
-    
+    <div>
+       <h1 className="text-3xl text-centre font-bold underline">
+      Hello world!
+    </h1>
     </div>
   );
 }

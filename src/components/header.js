f9b62127b0210c711e7ebb088a6ab63bd7daf4b6
treva556 <marktreva92@gmail.com>
2023-12-05 15:56:06 +0300

    new

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,15 @@
+
+
+// code 
 import React from "react";
+import pic11 from "../images/pic11.jpeg";
 
 function Header() {
   return (
     <div>
       <div className="w-screen h-[1cm] bg-black text-white text-center">
         <p className="text-center">BR</p>
+        <img src={pic11} alt="nal" />
       </div>
     </div>
   );
